2ef347430ee319f00453c8ec19997acdc2e2c03b
Cowington Post <cowingtonpost@gmail.com>
2023-03-05 11:09:26 -0800

    rename crate

--- src/bin/bare-server-rs.rs
+++ src/bin/bare-server-rs.rs
@@ -5,7 +5,7 @@ use actix_web::{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(bare_server_cow::configure))
+    HttpServer::new(|| App::new().configure(bare_server_rs::configure))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
